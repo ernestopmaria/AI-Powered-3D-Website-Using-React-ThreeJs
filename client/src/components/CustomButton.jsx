9a30ecc7730b467f982f5719012323ee99e4dd26
ernesto.maria <ernestomaria93@gmail.com>
2023-04-01 23:19:05 +0100

    canvas settings

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+
+const CustomButton = ({ type, customStyles, handleClick, title }) => {
+    const snap = useSnapshot(state)
+    const generateStyle = (types) => {
+        if (type === 'filled') {
+            return {
+                backgroundColor: snap.color,
+            color:'#fff'
+            }
+        }
+    }
+  return (
+      <button
+          className={`px-2  py-1.5 flex-1 rounded-md ${customStyles}`}
+          style={generateStyle(type)}
+          onClick={handleClick}
+      >{title}</button>
+  )
+}
+
+export default CustomButton
